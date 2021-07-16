d30b5e4099585677011b36dd76d6436509497eb8
Steven <stevenbellamy0@gmail.com>
2021-07-17 00:23:18 +1200

    Hello world

---
+++ components/ProjectDetails.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="flex flex-col justify-between flex-grow">
+    <div>
+      <p>{{ explanation }}</p>
+
+      <div class="mt-7 mb-4">
+        <h3 class="text-xl font-semibold">What was my involvement in the project?</h3>
+      </div>
+
+      <p>{{ involvementDesc }}</p>
+
+      <ul class="list-disc my-4 ml-4">
+        <li v-for="involvementListItem in involvementList">{{ involvementListItem }}</li>
+      </ul>
+    </div>
+
+    <div class="pt-4 flex">
+      <a :href="projectUrl" target="_blank" class="text-blue-600 underline">Project website</a>
+      <div class="ml-4">
+        <a :href="sourceUrl"
+           target="_blank"
+           class="text-blue-600 underline"
+           v-if="sourceUrl !== ''">Source</a>
+        <p class="text-gray-400 underline"
+           v-else>Source not available</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    explanation: {
+      type: String,
+      required: true
+    },
+    involvementDesc: {
+      type: String,
+      required: true
+    },
+    involvementList: {
+      type: Array as Array<String>,
+      default: []
+    },
+    projectUrl: {
+      type: String,
+      required: true
+    },
+    sourceUrl: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="max-w-5xl mx-auto px-6">
+    <project title="Waxeye"
+             image-url="/images/waxeye-project.jpg"
+             image-alt="Waxeye website">
+      <project-details project-url="https://waxeye.co.nz"
+                       explanation="Waxeye is a digital agency centered around web design/development, VR, AR, MR. The website showcases their case studies from various clients they have worked with over the years. Waxeye website features several pages through its hamburger menu and a complex CMS driven case study component implementation."
+                       involvement-desc="Initially the project was migrated / rebuilt from a previous system, I worked with design and test to produce a product which included:"
+                       :involvement-list="[
+                           'Layout system which included header, footer an elaborate hamburger menu that resizes properly depending on the device',
+                           'Contact page with frontend / backend validation, reCaptcha and sending success emails',
+                           'Complex component based case studies, these allow for putting a library of components on the page in any order. This also included dark '
+                       ]"></project-details>
+    </project>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import ProjectDetails from '~/components/ProjectDetails.vue';
+import Project from "~/components/Project.vue"
+
+export default Vue.extend({
+  components: { Project, ProjectDetails }
+})
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
